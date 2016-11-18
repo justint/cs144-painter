8441c548d8c0d63cadb613c6042fe6a64ddec16d
Justin Tennant <justin.tennant@sjsu.edu>
2016-11-17 16:41:59 -0800

    Set up Xcode project

---
+++ cs144-painter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cs144-painter
+//
+//  Created by Justin Tennant on 11/17/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
